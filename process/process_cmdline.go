311050f18a745363c77788a9aeb98cfe85d389fb
Richard Conway <richard@rconway.co.uk>
2021-07-20 17:21:18 +0100

    Add 'process connect' sub-command

--- process/process_cmdline.go
+++ process/process_cmdline.go
@@ -2,12 +2,32 @@ package process
 
 import (
 	"flag"
+	"fmt"
 	"io"
+	"path/filepath"
+
+	"github.com/rconway/eogo/utils"
 )
 
-func ProcessCmdlineHandler(args []string, w io.Writer) error {
-	flagSet := flag.NewFlagSet("process", flag.ExitOnError)
+func usage(args []string, commands *utils.CommandMap, w io.Writer) {
+	fmt.Fprintf(w, "Usage:\n%v <command> <options>\n", filepath.Base(args[0]))
+	fmt.Fprintln(w, "  <command> is one of...")
+	for k, v := range *commands {
+		fmt.Fprintf(w, "    %v: %v\n", k, v.Description)
+	}
+	fmt.Fprintln(w, "  <options> varies for each command")
+}
+
+func ProcessConnectCmdlineHandler(args []string, w io.Writer) error {
+	flagSet := flag.NewFlagSet("connect", flag.ExitOnError)
 	var serverUrl string
-	flagSet.StringVar(&serverUrl, "server", "http://localhost", "Specify the Process server URL")
+	flagSet.StringVar(&serverUrl, "server", "http://localhost", "Specify the Processing server URL")
 	return flagSet.Parse(args)
+}
+
+func ProcessCmdlineHandler(args []string, w io.Writer) error {
+	commands := utils.CommandMap{
+		"connect": utils.CommandDetails{ProcessConnectCmdlineHandler, "connect to the Processing server"},
+	}
+	return utils.HandleCmdline(args, &commands, usage, w)
 }
